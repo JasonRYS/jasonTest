6311e791f414153378719fd4d598829e6c313400
Jason <1063343352@qq.com>
2017-04-15 18:17:56 +0800

    221321

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(function(){	
+	var b = -300;
+	$("h1").click(function(){	
+		var ithis = $(this);	
+		console.log(b);
+		if (b*(-1) > 0) {
+			b = 300;
+		}else{
+			b = -300;
+		}
+		ithis.animate({
+			top:b*(-1)+"px"
+		},1000)	
+	})
+
+})
